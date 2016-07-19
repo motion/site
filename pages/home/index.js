160184cc1a216ed8ddfabaad0483ab4201d7f8a2
natew <natewienert@gmail.com>
2016-07-19 00:48:54 -0700

    tweaks

--- pages/home/index.js
+++ pages/home/index.js
@@ -38,7 +38,7 @@ export default @component class Root {
             <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
           </Flex>
           <Flex>
-            <img src="screen.png" width={2782 / 2} height={1734 / 2} />
+            <img src="screen.png" width={2782 / 3} height={1734 / 3} />
           </Flex>
         </Flex>
       </Section>
